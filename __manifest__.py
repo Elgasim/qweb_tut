828dcc3c343983e5ce1913f16f0be4a07449e9a6
mohammed <elgasim.watt1994@gmail.com>
2024-11-17 10:28:43 +0200

    Start up the project

---
+++ __manifest__.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+# Part of Odoo. See LICENSE file for full copyright and licensing details.
+
+{
+    'name': 'Qweb Tutorials',
+    'version': '1.0',
+    'category': 'Technical',
+    'summary': 'Qweb Template Tut',
+    'depends': ['base'],
+    "data": [
+    ],
+    'demo': [
+    ],
+    'css': [],
+    'installable': True,
+    'auto_install': False,
+    'application': True,
+    'license': 'LGPL-3',
+}
